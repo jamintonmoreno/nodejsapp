95ad4cefb9e8a213181a7f9aa03b7d7f741c9f61
Jaminton Moreno <74082502+jamintonmoreno@users.noreply.github.com>
2024-03-25 20:20:27 -0500

    Update nodejsDockerDSL.groovy

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -16,7 +16,7 @@ job('Aplicacion Node.js Docker DSL') {
         dockerBuildAndPublish {
             repositoryName('jamintonmoreno/nodejsapp')
             tag('${GIT_REVISION,length=7}')
-            registryCredentials('docker-hub')
+            registryCredentials('jamintonmoreno')
             forcePull(false)
             createFingerprints(false)
             skipDecorate()
