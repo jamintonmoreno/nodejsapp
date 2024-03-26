83a80ae5ded03cc52cde36a4750ebeab05a1b1f5
Jaminton Moreno <74082502+jamintonmoreno@users.noreply.github.com>
2024-03-25 20:15:28 -0500

    Update nodejsDockerDSL.groovy
    upgrade

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js Docker DSL') {
     description('Aplicación Node JS Docker DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/jamintonmoreno/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('jamintonmoreno')
+            node / gitConfigEmail('jaminton.morenof@gmail.com')
         }
     }
     triggers {
